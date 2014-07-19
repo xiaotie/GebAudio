921e8bbed2d1f4ec6fff93a0bb1c2d649e96225a
xiaotie <xiaotie@geblab.com>
2014-07-19 10:43:37 +0800

    添加 AbstractVoiceActivityDetector 类

---
+++ src/Geb.Audio/FrontEnds/EndPoint/AbstractVoiceActivityDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geb.Audio.FrontEnds.EndPoint
+{
+    public abstract class AbstractVoiceActivityDetector : BaseDataProcessor
+    {
+        /// <summary>
+        /// Returns the state of speech detected.
+        /// </summary>
+        /// <returns>if last processed data object was classified as speech</returns>
+        public abstract Boolean isSpeech();
+    }
+}
